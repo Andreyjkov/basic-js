7e4f5fe56a05ff59edc93a5fc15436345e5d6c92
Andreyjkov <andreyjs@gmail.com>
2022-04-29 23:37:47 +0300

    feat:add task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (Array.isArray(members) === false ){
+    return false
+  }
+  let nameTime = []; 
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === "string") {
+      const tNameTeam = members[i].trim();
+      nameTime.push(tNameTeam[0].toUpperCase());
+    }
+  }
+  return nameTime.sort().join("");
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function sortByHeight(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let temp = arr.filter((el) => {
+    if (el !== -1) {
+      return el;
+    }
+  });
+  temp.sort((a, b) => {
+    return a - b;
+  });
+
+  let index = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      arr[i] = temp[index];
+      index++;
+    }
+  }
+  return arr;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
